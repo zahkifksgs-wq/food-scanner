8836974a260c054e6c63760a1000da2d151bba98
zahkifksgs-wq <zahkifksgs@gmail.com>
2026-09-07 14:35:00 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -117,6 +117,4 @@ app.get('/api/barcode/:code', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`سروَر کامیابی سے جاری ہے: http://localhost:${PORT}`);
-});
+app.listen(module.exports = app;);
